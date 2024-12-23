257f7a517e688729f89dd770d1a583e308ac3b5b
Ruslan <Giovingo92@gmail.com>
2024-12-23 20:21:56 +0200

    feat: UI login auth

--- app/auth/signup/create-user.ts
+++ app/auth/signup/create-user.ts
@@ -1,10 +1,14 @@
 'use server'
 
+import { FormError } from '@/app/common/form-error.interface'
 import { redirect } from 'next/navigation'
 import { post } from '@/app/utils/fetch'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default async function createUser(_prevState: any, formData: FormData) {
+export default async function createUser(
+	_prevState: FormError,
+	formData: FormData
+) {
 	const { error } = await post('users', formData)
 
 	if (error) {

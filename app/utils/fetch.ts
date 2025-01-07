d22ea394fa6eaa03a6e209a6e06f353e2a619c9b
Ruslan <Giovingo92@gmail.com>
2025-01-07 20:14:04 +0200

    feat: server cookies

--- app/utils/fetch.ts
+++ app/utils/fetch.ts
@@ -1,10 +1,19 @@
+import { cookies } from 'next/headers'
 import { API_URL } from '../constants/api'
 import { getErrorMessage } from './errors'
 
+const getHeaders = async () => {
+	const cookiesData = await cookies()
+	return {
+		Cookie: cookiesData.toString(),
+	}
+}
+
 export const post = async (path: string, formData: FormData) => {
+	const headers = await getHeaders()
 	const res = await fetch(`${API_URL}/${path}`, {
 		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
+		headers: { 'Content-Type': 'application/json', ...headers },
 		body: JSON.stringify(Object.fromEntries(formData)),
 	})
 
@@ -15,4 +24,12 @@ export const post = async (path: string, formData: FormData) => {
 	}
 
 	return { error: '' }
+}
+
+export const get = async (path: string) => {
+	const headers = await getHeaders()
+	const res = await fetch(`${API_URL}/${path}`, {
+		headers: { ...headers },
+	})
+	return res.json()
 }

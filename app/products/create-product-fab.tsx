dad37029aef25d39986a995665a209f8af51ba2b
Ruslan <Giovingo92@gmail.com>
2025-01-16 18:22:21 +0200

    feat: create product fab page

---
+++ app/products/create-product-fab.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { Fab } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
+
+export default function CreateProductFab() {
+	return (
+		<>
+			<div className='absolute left-10 bottom-10'>
+				<Fab color='primary'>
+					<AddIcon />
+				</Fab>
+			</div>
+		</>
+	)
+}

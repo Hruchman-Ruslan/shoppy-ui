59b718ab971345d474b012204764ca5150eb9ef8
Ruslan <Giovingo92@gmail.com>
2025-01-16 18:35:25 +0200

    feat: create product modal

--- app/products/create-product-fab.tsx
+++ app/products/create-product-fab.tsx
@@ -2,12 +2,20 @@
 
 import { Fab } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
+import CreateProductModal from './create-product-modal'
+import { useState } from 'react'
 
 export default function CreateProductFab() {
+	const [modalVisible, setModalVisible] = useState(false)
+
 	return (
 		<>
+			<CreateProductModal
+				open={modalVisible}
+				handleClose={() => setModalVisible(false)}
+			/>
 			<div className='absolute left-10 bottom-10'>
-				<Fab color='primary'>
+				<Fab color='primary' onClick={() => setModalVisible(true)}>
 					<AddIcon />
 				</Fab>
 			</div>
